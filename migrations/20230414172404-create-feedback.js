b8a88a32a5433da9309b0d0f9882f6bcafef3dfa
danish-butt-veetech <danish.butt@veetech.ai>
2023-04-14 22:25:05 +0500

    feedback tables creation

---
+++ migrations/20230414172404-create-feedback.js
@@ -0,0 +1,55 @@
+
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable("Feedbacks", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      gc_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Courses',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      org_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Organizations',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      contact_medium: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable("Feedbacks");
+  },
+};
+
+
+
