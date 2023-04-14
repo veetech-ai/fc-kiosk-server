31299a1835f858ca5a299e923f91d4b6a00d4859
danish-butt-veetech <danish.butt@veetech.ai>
2023-04-14 22:22:42 +0500

    created FAQ migrations

---
+++ migrations/20230414172207-create-faq.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable("FAQ", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      question: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      answer: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      gc_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Courses',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable("FAQ");
+  },
+};
+
