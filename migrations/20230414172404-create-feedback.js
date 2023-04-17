1f224348cbfc8ec55c694a0555730325ca7791b2
usman najmi <usman.najmi.cowlar@gmail.com>
2023-04-17 22:57:07 +0500

    index rating attribute

--- migrations/20230414172404-create-feedback.js
+++ migrations/20230414172404-create-feedback.js
@@ -3,46 +3,49 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.createTable("Feedbacks", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      gc_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Courses",
-          key: "id",
+    return Promise.all([
+      await queryInterface.createTable("Feedbacks", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE",
-      },
-      org_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Organizations",
-          key: "id",
+        gc_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Courses",
+            key: "id",
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
         },
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE",
-      },
-      phone: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      rating: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      contact_medium: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-    });
+        org_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Organizations",
+            key: "id",
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        },
+        phone: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        rating: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        contact_medium: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+      }),
+      queryInterface.addIndex("Feedbacks", ["rating"]),
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
