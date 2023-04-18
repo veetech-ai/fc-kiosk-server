e3111a4ce7bf828cae5fb94835dbb5a2b764d757
usman najmi <usman.najmi.cowlar@gmail.com>
2023-04-18 19:59:43 +0500

    liniting

--- migrations/20230418132212-create-coach.js
+++ migrations/20230418132212-create-coach.js
@@ -54,13 +54,13 @@ module.exports = {
         createdAt: {
           allowNull: false,
           type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
         updatedAt: {
           allowNull: false,
           type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-          onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+          onUpdate: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
       }),
       queryInterface.addIndex("Coaches", ["name"]),
