2643bf70e92d49d30a2e3374e9306ea8e90404cc
usman najmi <usman.najmi.cowlar@gmail.com>
2023-04-27 15:38:10 +0500

    linting

--- migrations/20230427082212-add-golfbert-id-column-in-courses-table.js
+++ migrations/20230427082212-add-golfbert-id-column-in-courses-table.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.addColumn("Courses", "golfbert_id", {
       type: Sequelize.INTEGER,
       allowNull: true,
     });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn("Courses", "golfbert_id");
-  }
+  },
 };
