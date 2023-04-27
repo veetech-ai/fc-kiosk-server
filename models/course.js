23676e2486ffa8e14ee0a8b1ec5ac4238a16f2e4
usman najmi <usman.najmi.cowlar@gmail.com>
2023-04-27 16:44:39 +0500

    change in model

--- models/course.js
+++ models/course.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       golfbertId: {
         field: "golfbert_id",
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         onUpdate: "CASCADE",
         onDelete: "CASCADE",
       },
