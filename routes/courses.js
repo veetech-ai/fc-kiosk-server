2853533429bbd83202d997194ac069a14535c35c
sami-ullah-veetech <129540477+sami-ullah-veetech@users.noreply.github.com>
2023-04-27 16:00:33 +0500

    Feat/nearby-golf-courses-basic-name-search-not-fuzzy (#53)
    * addition of conversion and geo location packages
    
    * name and nearby search implemented
    
    * chore:linting
    
    * sorted the resulting array based on distance
    
    ---------
    
    Co-authored-by: sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>

---
+++ routes/courses.js
@@ -0,0 +1,12 @@
+const config = require("../config/config");
+const CoursesController = require("../controllers/courses/courses");
+const validation_middleware = require("../middlewares/auth.validation");
+
+exports.routesConfig = function (app, router) {
+  const courses = `${config.app.apiPath}courses`;
+
+  router.get(courses, [
+    validation_middleware.validJWTNeeded,
+    CoursesController.get_courses,
+  ]);
+};
