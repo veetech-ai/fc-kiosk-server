3f4ce9cc95dd77068415bfcbd3c572289c245922
usman najmi <usman.najmi.cowlar@gmail.com>
2023-05-01 16:00:55 +0500

    wip

---
+++ migrations/20230501095800-create-mobile-courses-table.js
@@ -0,0 +1,73 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return Promise.all([
+      await queryInterface.createTable("Mobile_Courses", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        golfbert_id: {
+          allowNull: true,
+          type: Sequelize.INTEGER,
+          unique: true,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null,
+        },
+        phone: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null,
+        },
+        country: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null,
+        },
+        street: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null,
+        },
+        city: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null,
+        },
+        state: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null,
+        },
+        zip: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null,
+        },
+        lat: {
+          type: Sequelize.DOUBLE,
+          allowNull: true,
+        },
+        long: {
+          type: Sequelize.DOUBLE,
+          allowNull: true,
+        },
+      }),
+      await queryInterface.addIndex("Mobile_Courses", ["lat"]),
+      await queryInterface.addIndex("Mobile_Courses", ["long"]),
+      await queryInterface.addIndex("Mobile_Courses", ["name"]),
+      await queryInterface.addIndex("Mobile_Courses", ["golfbert_id"]),
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable("Mobile_Courses");
+  },
+};
