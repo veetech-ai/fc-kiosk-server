a83b09e490a846cb4b9a0206afd8483345b70142
sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>
2023-05-01 22:14:53 +0500

    url to api taken to services for golfbert APIs

--- services/golfbert/golfbert.js
+++ services/golfbert/golfbert.js
@@ -1,57 +1,81 @@
 // Common Imports
 const helper = require("../../common/helper");
 
-exports.get_courses = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+const BASE_URL = "https://api.golfbert.com/v1";
+
+exports.get_courses = async (queryParams = {}) => {
+  const API_URL = BASE_URL + "/courses";
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_course_by_id = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_course_by_id = async (courseId, queryParams = {}) => {
+  const API_URL = `${BASE_URL}/courses/${courseId}`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_holes_by_courseId = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_holes_by_courseId = async (courseId, queryParams = {}) => {
+  const API_URL = BASE_URL + `/courses/${courseId}/holes`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_scorecard_by_courseId = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_scorecard_by_courseId = async (courseId, queryParams = {}) => {
+  const API_URL = BASE_URL + `/courses/${courseId}/scorecard`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_teeboxes_by_courseId = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_teeboxes_by_courseId = async (courseId, queryParams = {}) => {
+  const API_URL = BASE_URL + `/courses/${courseId}/teeboxes`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_holes = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_holes = async (queryParams = {}) => {
+  const API_URL = BASE_URL + `/holes`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_holes_by_holeId = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_holes_by_holeId = async (holeId, queryParams = {}) => {
+  const API_URL = BASE_URL + `/holes/${holeId}`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_polygons_by_holeId = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_polygons_by_holeId = async (holeId, queryParams = {}) => {
+  const API_URL = BASE_URL + `/holes/${holeId}/polygons`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_teeboxes_by_holeId = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_teeboxes_by_holeId = async (holeId, queryParams = {}) => {
+  const API_URL = BASE_URL + `/holes/${holeId}/teeboxes`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_teeboxcolors = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_teeboxcolors = async (queryParams = {}) => {
+  const API_URL = BASE_URL + `/teeboxcolors`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
 
-exports.get_teeboxtypes = async (queryParams, apiUrl) => {
-  const response = await helper.call_golfbert_api(apiUrl, queryParams);
+exports.get_teeboxtypes = async (queryParams = {}) => {
+  const API_URL = BASE_URL + `/teeboxtypes`;
+
+  const response = await helper.call_golfbert_api(API_URL, queryParams);
   return response.data;
 };
