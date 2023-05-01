be3723a504ddce8517d8a741affc642058e52369
usman najmi <usman.najmi.cowlar@gmail.com>
2023-05-01 14:37:54 +0500

    typo fixed

--- migrations/20230501093136-remove-gc-id-from-holes.js
+++ migrations/20230501093136-remove-gc-id-from-holes.js
@@ -3,12 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // Remove the gc_id column from the Games table
+    // Remove the gc_id column from the Holes table
     await queryInterface.removeColumn("Holes", "gc_id");
   },
 
   async down(queryInterface, Sequelize) {
-    // Add the gc_id column back to the Games table
+    // Add the gc_id column back to the Holes table
     await queryInterface.addColumn("Holes", "gc_id", {
       type: Sequelize.INTEGER,
       allowNull: false,
