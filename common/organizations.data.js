6e39ad8331e5e42d6b4fe497758fabcbfa1e88e8
kausar-ali-veetech <kausar.ali@veetech.ai>
2023-05-04 12:56:43 +0500

    refactor: seeder

--- common/organizations.data.js
+++ common/organizations.data.js
@@ -1,18 +1,36 @@
-const organizationsInApplication = {
+const config = require("../config/config");
+
+let organizationsInApplication = {
   test: {
+    id: 1,
     name: "Test",
     description: "Test organization for testing ",
   },
   veetech: {
+    id: 2,
     name: "Veetech",
     description: "Test organization for testing ",
   },
   golfers: {
+    id: 3,
     name: "Golfers",
     description: "Digital Fairways Mobile App Users",
   },
 };
 
+const testOrganizations = {
+  zong: {
+    id: 50,
+    name: "Zong",
+    description: "Test organization for testing",
+  },
+};
+
+if (config.env == "test")
+  organizationsInApplication = {
+    ...organizationsInApplication,
+    ...testOrganizations,
+  };
 const organizationsData = Object.values(organizationsInApplication);
 
 module.exports = {
