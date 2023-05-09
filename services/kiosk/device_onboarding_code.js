96e12b156f0d8ed6ea8a4ec029eda7a9ed8718b3
sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>
2023-05-09 22:55:36 +0500

    Creation of services for device onboarding

--- services/kiosk/device_onboarding_code.js
+++ services/kiosk/device_onboarding_code.js
@@ -4,15 +4,21 @@ const models = require("../../models/index");
 const ServiceError = require("../../utils/serviceError");
 const DeviceOnboardingCode = models.DeviceOnboardingCode;
 
+function generateDeviceOnboardingCode() {
+    const code = helper.generate_random_string({
+        length: config.auth.kioskOnboardingAuth.otpLength,
+        type: "numeric",
+    });
+    return code
+}
+
 async function createDeviceOnboardingCode() {
-  const code = helper.generate_random_string({
-    length: config.auth.kioskOnboardingAuth.otpLength,
-    type: "numeric",
-  });
-  const createdCode = await DeviceOnboardingCode.create({ code });
-  if (!createdCode)
-    ServiceError("Unable to Create Device Onboarding code", 500);
-  return createdCode;
+    const code = generateDeviceOnboardingCode()
+    const createdCode = await DeviceOnboardingCode.create({ code });
+    if (!createdCode) {
+        throw new ServiceError("Unable to Create Device Onboarding code", 500);
+    }
+    return createdCode;
 }
 
 async function createDeviceOnboardingCodeIfNotCreated() {
@@ -20,6 +26,19 @@ async function createDeviceOnboardingCodeIfNotCreated() {
   if (!existingCode) await createDeviceOnboardingCode();
 }
 
+async function refreshDeviceOnboardingCode() {
+  const existingCode = await DeviceOnboardingCode.findOne({});
+  if (!existingCode) {
+    throw new ServiceError("No device onboarding code exists", 404);
+  };
+  const newCode = generateDeviceOnboardingCode()
+  const updatedCode = await existingCode.update({code: newCode})
+
+  return updatedCode;
+}
+
 module.exports = {
   createDeviceOnboardingCodeIfNotCreated,
+  createDeviceOnboardingCode,
+  refreshDeviceOnboardingCode,
 };
