625160dc6a382383ba3140caddbc0515b66ecd09
kausar-ali-veetech <kausar.ali@veetech.ai>
2023-05-15 16:20:13 +0500

    revert the migrations

---
+++ migrations/20200310084056-create-coupons.js
@@ -0,0 +1,97 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Coupons", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        comment: "Coupon title",
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+        comment: "Coupon description",
+      },
+      expiry: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        comment: "Coupon expiry date. after this it will worthless",
+      },
+      code: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        comment: "Coupon code",
+      },
+      discount_type: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+        comment: "Coupon discount type. 0=fixed, 1=percentage. default is 0",
+      },
+      discount: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        comment: "Coupon discount rate",
+      },
+      max_use_limit: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: null,
+        comment:
+          "Coupon maximum use limit. default value is null, means limited user can use it.",
+      },
+      used_by: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+        comment:
+          "Coupon used by user(s). will tell us that how many users used this token.",
+      },
+      coupon_for: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        comment:
+          "Specify that this coupon is for users or device type. default value is 0. 0=users, 1=device_type",
+      },
+      users: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        comment:
+          "Coupon for specific user. contianer commma separated user IDs. default value is null, means for all users",
+      },
+      device_types: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        comment:
+          "Coupon for specific device type. contianer commma separated device type IDs. default value is null, means for all device type",
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        comment: "Coupon status",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Coupons");
+  },
+};
---
+++ migrations/20200617120722-create-coupon-used.js
@@ -0,0 +1,34 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Coupon_Useds", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      coupon_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Coupon_Useds");
+  },
+};
