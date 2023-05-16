ec46cf9632d051e6c37b81afeffe42c8e7d0c2c8
sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>
2023-05-16 14:29:34 +0500

    implementing FAQ

---
+++ controllers/kiosk/course-faqs.js
@@ -0,0 +1,310 @@
+// External Module Imports
+const Validator = require("validatorjs");
+const formidable = require("formidable");
+
+// Common Imports
+const apiResponse = require("../../common/api.response");
+const helper = require("../../common/helper");
+const upload_file = require("../../common/upload");
+// Logger Imports
+const courseService = require("../../services/kiosk/course");
+const courseFaqsService = require("../../services/kiosk/course_faqs");
+
+/**
+ * @swagger
+ * tags:
+ *   name: Course-Faqs
+ *   description: Golf Course's FAQs API's
+ */
+exports.createCourseShop = async (req, res) => {
+  /**
+   * @swagger
+   *
+   * /course-faqs:
+   *   post:
+   *     security:
+   *       - auth: []
+   *     description: create golf course Shop.
+   *     tags: [Course-Faqs]
+   *     consumes:
+   *       - application/x-www-form-urlencoded
+   *     parameters:
+   *       - name: gcId
+   *         description: golf course for which shop is being created
+   *         in: formData
+   *         required: true
+   *         type: integer
+   *       - name: name
+   *         description: name of the shop
+   *         in: formData
+   *         required: true
+   *         type: string
+   *       - name: subheading
+   *         description: subheading briefly describing the shop
+   *         in: formData
+   *         required: true
+   *         type: string
+   *       - name: description
+   *         description: description of the shop
+   *         in: formData
+   *         required: true
+   *         type: string
+   *       - name: image
+   *         in: formData
+   *         description: Upload image of Golf course shop
+   *         required: true
+   *         type: file
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: success
+   */
+  try {
+    const form = new formidable.IncomingForm();
+
+    const { fields, files } = await new Promise((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err);
+        resolve({ fields, files });
+      });
+    });
+
+    const validation = new Validator(fields, {
+      gcId: "required|integer",
+      name: "required|string",
+      subheading: "required|string",
+      description: "required|string",
+    });
+
+    if (validation.fails()) {
+      return apiResponse.fail(res, validation.errors);
+    }
+
+    const courseId = fields.gcId;
+    const course = await courseService.getCourseById(courseId);
+
+    const loggedInUserOrgId = req.user.orgId;
+    const isSuperOrAdmin = helper.hasProvidedRoleRights(req.user.role, [
+      "super",
+      "admin",
+    ]).success;
+    if (!isSuperOrAdmin && loggedInUserOrgId !== course.orgId) {
+      return apiResponse.fail(res, "Course not Found", 404);
+    }
+
+    const shopImage = files?.image;
+    const imageIdentifier = await upload_file.uploadImageForCourse(
+      shopImage,
+      courseId,
+    );
+    const reqBody = { ...fields, image: imageIdentifier };
+    const courseShop = await courseFaqsService.createCourseShop(
+      reqBody,
+      course.orgId,
+    );
+
+    const imageUrl = upload_file.getFileURL(courseShop.image);
+    courseShop.setDataValue("image", imageUrl);
+
+    return apiResponse.success(res, req, courseShop);
+  } catch (error) {
+    return apiResponse.fail(res, error.message, error.statusCode || 500);
+  }
+};
+
+exports.getCourseFaqs = async (req, res) => {
+  /**
+   * @swagger
+   *
+   * /course-faqs/courses/{courseId}:
+   *   get:
+   *     security:
+   *       - auth: []
+   *     description: Get faqs for a specific course.
+   *     tags: [Course-Faqs]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: courseId
+   *         description: Golf Course ID
+   *         in: path
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: Success
+   */
+
+  try {
+    const courseId = Number(req.params.courseId);
+    if (!courseId) {
+      return apiResponse.fail(res, "courseId must be a valid number");
+    }
+
+    const course = await courseService.getCourseById(courseId);
+
+    const loggedInUserOrgId = req.user.orgId;
+    const isSuperOrAdmin = helper.hasProvidedRoleRights(req.user.role, [
+      "super",
+      "admin",
+    ]).success;
+    if (!isSuperOrAdmin && loggedInUserOrgId !== course.orgId) {
+      return apiResponse.fail(res, "Course not Found", 404);
+    }
+
+    const courseFaqs = await courseFaqsService.getCourseFaqs(courseId);
+
+    return apiResponse.success(res, req, courseFaqs);
+  } catch (error) {
+    return apiResponse.fail(res, error.message, error.statusCode || 500);
+  }
+};
+
+exports.updateCourseShop = async (req, res) => {
+  /**
+   * @swagger
+   * /course-faqs/{shopId}:
+   *   patch:
+   *     security:
+   *       - auth: []
+   *     description: update shop for a golf course.
+   *     tags: [Course-Faqs]
+   *     consumes:
+   *       - multipart/form-data
+   *     parameters:
+   *       - in: path
+   *         name: shopId
+   *         description: id of course
+   *         required: true
+   *         type: integer
+   *       - name: name
+   *         description: name of the shop
+   *         in: formData
+   *         required: false
+   *         type: string
+   *       - name: subheading
+   *         description: subheading briefly describing the shop
+   *         in: formData
+   *         required: false
+   *         type: string
+   *       - name: description
+   *         description: description of the shop
+   *         in: formData
+   *         required: false
+   *         type: string
+   *       - name: image
+   *         in: formData
+   *         description: Upload image of Golf course shop
+   *         required: false
+   *         type: file
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: success
+   */
+
+  try {
+    const form = new formidable.IncomingForm();
+
+    const { fields, files } = await new Promise((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err);
+        resolve({ fields, files });
+      });
+    });
+
+    const validation = new Validator(fields, {
+      name: "string",
+      subheading: "string",
+      description: "string",
+    });
+
+    if (validation.fails()) {
+      return apiResponse.fail(res, validation.errors);
+    }
+
+    const shopId = req.params.shopId;
+    const courseShop = await courseFaqsService.getCourseShopById(shopId);
+
+    const loggedInUserOrgId = req.user.orgId;
+    const isSuperOrAdmin = helper.hasProvidedRoleRights(req.user.role, [
+      "super",
+      "admin",
+    ]).success;
+    if (!isSuperOrAdmin && loggedInUserOrgId !== courseShop.orgId) {
+      return apiResponse.fail(res, "Shop not Found", 404);
+    }
+
+    const reqBody = { ...fields };
+    const shopImage = files?.image;
+    if (shopImage) {
+      const imageIdentifier = await upload_file.uploadImageForCourse(
+        shopImage,
+        courseShop.gcId,
+      );
+      reqBody.image = imageIdentifier;
+    }
+    const updatedCourseShop = await courseFaqsService.updateCourseShop(
+      shopId,
+      reqBody,
+    );
+
+    if (updatedCourseShop) {
+      const imageUrl = upload_file.getFileURL(updatedCourseShop.image);
+      updatedCourseShop.setDataValue("image", imageUrl);
+    }
+
+    return apiResponse.success(res, req, updatedCourseShop);
+  } catch (error) {
+    return apiResponse.fail(res, error.message, error.statusCode || 500);
+  }
+};
+
+exports.deleteCourseShop = async (req, res) => {
+  /**
+   * @swagger
+   *
+   * /course-faqs/{shopId}:
+   *   delete:
+   *     security:
+   *       - auth: []
+   *     description: Delete shop.
+   *     tags: [Course-Faqs]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: shopId
+   *         description: Organization ID
+   *         in: path
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: Success
+   */
+
+  try {
+    const shopId = Number(req.params.shopId);
+    if (!shopId) {
+      return apiResponse.fail(res, "shopId must be a valid number");
+    }
+
+    const courseShop = await courseFaqsService.getCourseShopById(shopId);
+
+    const loggedInUserOrgId = req.user.orgId;
+    const isSuperOrAdmin = helper.hasProvidedRoleRights(req.user.role, [
+      "super",
+      "admin",
+    ]).success;
+    if (!isSuperOrAdmin && loggedInUserOrgId !== courseShop.orgId) {
+      return apiResponse.fail(res, "Shop not Found", 404);
+    }
+
+    await courseFaqsService.deleteCourseShop(shopId);
+    return apiResponse.success(res, req, "Shop Deleted");
+  } catch (error) {
+    return apiResponse.fail(res, error.message, error.statusCode || 500);
+  }
+};
