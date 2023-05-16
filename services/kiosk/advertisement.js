11e2642e3582639489ac9d808351b822e5d4fc21
danish-butt-veetech <danish.butt@veetech.ai>
2023-05-16 10:30:54 +0500

    added service file to ads

---
+++ services/kiosk/advertisement.js
@@ -0,0 +1,81 @@
+const models = require("../../models/index");
+const ServiceError = require("../../utils/serviceError");
+const screenConfigServices = require("../screenConfig/screens");
+const membershipService = require("./membership");
+
+const Course = models.Course;
+const Organization = models.Organization;
+const Advertisements = models.Ads;
+
+async function createCourse(reqBody, orgId) {
+  // Check if organization exists with the specified org_id
+  const organization = await Organization.findOne({ where: { id: orgId } });
+  if (!organization) {
+    throw new ServiceError(`Organization not found`, 404);
+  }
+
+  // Create a new course record
+  const course = await Course.create({
+    ...reqBody,
+    orgId,
+  });
+
+  // Create Screen Config to allow toggling visibility of content sections on kiosk
+  const gcId = course.id;
+  await screenConfigServices.createScreenConfig(gcId, orgId);
+  await membershipService.createMembership(gcId, orgId);
+
+  return course;
+}
+async function getCoursesByOrganization(orgId) {
+  // Check if organization exists with the specified org_id
+  const organization = await Organization.findOne({ where: { id: orgId } });
+  if (!organization) {
+    throw new ServiceError(`Organization not found`, 404);
+  }
+
+  // Find course record
+  const courses = await Course.findAll({
+    where: {
+      orgId,
+    },
+  });
+
+  return courses;
+}
+async function createCourseInfo(reqBody, courseId) {
+  // Check if organization exists with the specified org_id
+  // Create a new course record
+  const updatedCourse = await Course.update(
+    {
+      ...reqBody,
+    },
+    { where: { id: courseId } },
+  );
+  if (!updatedCourse[0])
+    throw new ServiceError("There is a problem. Please try later.");
+  return updatedCourse;
+}
+
+async function getCourseById(courseId) {
+  const course = await Course.findOne({
+    where: {
+      id: courseId,
+    },
+    attributes: {
+      exclude: ["org_id"],
+    },
+  });
+
+  if (!course) {
+    throw new ServiceError("Course not found", 404);
+  }
+  return course;
+}
+
+module.exports = {
+  createCourse,
+  getCoursesByOrganization,
+  createCourseInfo,
+  getCourseById,
+};
