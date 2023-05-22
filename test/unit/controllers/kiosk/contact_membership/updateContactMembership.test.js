e755761f1b64218eb54f756c4fcbc3c1e44e56d7
Usman Najmi <najmirashdalita12345@gmail.com>
2023-05-23 02:09:41 +0500

    linting

--- test/unit/controllers/kiosk/contact_membership/updateContactMembership.test.js
+++ test/unit/controllers/kiosk/contact_membership/updateContactMembership.test.js
@@ -18,7 +18,7 @@ describe("PATCH /api/v1/course-membership/contacts/{id}", () => {
     phone: "43423423",
     contact_medium: "text",
   };
-  let contactMembershipId
+  let contactMembershipId;
 
   beforeAll(async () => {
     // Create some courses for the test organization
@@ -65,12 +65,12 @@ describe("PATCH /api/v1/course-membership/contacts/{id}", () => {
       token: adminToken,
     });
     deviceToken = device.body.data.Device.device_token.split(" ")[1];
-    const response=await helper.post_request_with_authorization({
+    const response = await helper.post_request_with_authorization({
       endpoint: `kiosk-content/memberships/contacts`,
       token: deviceToken,
       params: { membershipId, ...reqBody },
     });
-    contactMembershipId=response.body.data.id
+    contactMembershipId = response.body.data.id;
   });
 
   const makeApiRequest = async (id, body, token = adminToken) => {
@@ -92,14 +92,22 @@ describe("PATCH /api/v1/course-membership/contacts/{id}", () => {
     const body = {
       isAddressed: true,
     };
-    const response = await makeApiRequest(contactMembershipId, body, customerToken);
+    const response = await makeApiRequest(
+      contactMembershipId,
+      body,
+      customerToken,
+    );
     expect(response.body.data).toBe("Updated Successfully");
   });
   it("should return validation error if non boolean value is passed", async () => {
     const body = {
       isAddressed: null,
     };
-    const response = await makeApiRequest(contactMembershipId, body, customerToken);
+    const response = await makeApiRequest(
+      contactMembershipId,
+      body,
+      customerToken,
+    );
     expect(response.body.data).toBe("isAddressed must be a boolean");
   });
   it("should return error while the api is being accessed by the customer of different organization", async () => {
