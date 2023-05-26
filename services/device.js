4c21d91acb6c9ea560cdbb86f9f38252e93f73f9
sami-ullah-veetech <129540477+sami-ullah-veetech@users.noreply.github.com>
2023-05-26 05:04:01 +0500

    retention made false for device reset and transfer (#132)
    Co-authored-by: sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>

--- services/device.js
+++ services/device.js
@@ -991,9 +991,13 @@ exports.reset = (device_id) => {
               helper.mqtt_publish_message(`d/${device_id}/ac/group`, {
                 group: null,
               });
-              helper.mqtt_publish_message(`d/${device_id}/gc`, {
-                gcId: null,
-              });
+              helper.mqtt_publish_message(
+                `d/${device_id}/gc`,
+                {
+                  gcId: null,
+                },
+                false,
+              );
               resolve(result);
             })
             .catch((err) => {
@@ -1131,9 +1135,13 @@ exports.verify_transfer_token = async (params, where) => {
   helper.mqtt_publish_message(`d/${device.id}/ac/sch`, {
     token: null,
   });
-  helper.mqtt_publish_message(`d/${device.id}/gc`, {
-    gcId: null,
-  });
+  helper.mqtt_publish_message(
+    `d/${device.id}/gc`,
+    {
+      gcId: null,
+    },
+    false,
+  );
 };
 
 exports.get_ownership_requests = (orgId) => {
