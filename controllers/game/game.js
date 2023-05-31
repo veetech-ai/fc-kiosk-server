b297c1d637461df969da1c47981a94c5b9403bb7
Maaz <maaz@veetech.ai>
2023-05-31 21:39:31 +0500

    feat: game controller

---
+++ controllers/game/game.js
@@ -0,0 +1,72 @@
+// External Module Imports
+const Validator = require("validatorjs");
+
+const apiResponse = require("../../common/api.response");
+const gameService = require("../../services/game/game");
+const {organizationsData} = require("../../common/organizations.data")
+const { v4: uuidv4 } = require("uuid");
+
+/**
+ * @swagger
+ * tags:
+ *   name: Game
+ *   description: Courses API's
+ */
+exports.create_game = async (req, res) => {
+  /**
+   * @swagger
+   *
+   * /game/:
+   *   post:
+   *     security:
+   *       - auth: []
+   *     description: logged In user can start/create game.
+   *     tags: [Game]
+   *     consumes:
+   *       - application/x-www-form-urlencoded
+   *     parameters:
+   *       - name: mcId
+   *         description: Course ID
+   *         in: body
+   *         required: true
+   *         type: string
+   *       - name: totalIdealShots
+   *         description: Course ID
+   *         in: body
+   *         required: true
+   *         type: string
+   *       - name: teeColor
+   *         description: Course ID
+   *         in: body
+   *         required: true
+   *         type: string
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: success
+   */
+  try {
+    const validation = new Validator(req.body, {
+      mcId: "required|integer",
+      totalIdealShots: "required|integer",
+      teeColor: "required|string",
+    });
+
+    if (validation.fails()) {
+      return apiResponse.fail(res, validation.errors);
+    }
+
+    req.body.ownerId = req.user.id;
+    req.body.participantId = req.user.id;
+    req.body.participantName = req.user.name;
+    req.body.startTime = new Date();
+    req.body.gameId = uuidv4();
+    req.body.orgId = req.user.orgId;
+
+    const createdGame = await gameService.createGame(req?.body)
+    return apiResponse.success(res, req, createdGame);
+  } catch (error) {
+    return apiResponse.fail(res, error.message, error.statusCode || 500);
+  }
+};
