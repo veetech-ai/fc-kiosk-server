568345ea5520be2ca77dc59e4d8a6c214e5702e3
Maaz <maaz@veetech.ai>
2023-05-31 18:51:58 +0500

    chore: new column tee color

---
+++ migrations/20230531134805-add-game-tee-color.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn("Games", "tee_color", {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.removeColumn("Games", "tee_color"),
+    ]);
+  },
+};
