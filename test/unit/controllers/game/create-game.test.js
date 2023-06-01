dbed8a09bb686ce0eb3234389dc8e796a8ceb45a
Maaz <maaz@veetech.ai>
2023-06-01 14:19:59 +0500

    feat: game APi test cases

--- test/unit/controllers/game/create-game.test.js
+++ test/unit/controllers/game/create-game.test.js
@@ -1,79 +1,142 @@
 const helper = require("../../../helper");
 const models = require("../../../../models/index");
+const config = require("../../../../config/config");
 const jwt = require("jsonwebtoken");
 const Course = models.Course;
 
 describe("Post: /game", () => {
-    let adminToken;
-    let customerToken;
-    let testManagerToken;
-    let differentOrganizationCustomerToken;
     let golferToken;
     let testOrganizationId = 1;
     let createdCourses;
+    let golferUser;
 
     beforeAll(async () => {
         // Create some courses for the test organization
+        
+        golferToken = await helper.get_token_for("golfer");
+        golferUser = jwt.decode(golferToken);
+        
         const courses = [
           {
             name: "Course 1",
             city: "Test City 1",
             state: "Test State 1",
-            org_id: testOrganizationId,
+            org_id: golferUser.orgId,
           },
           {
             name: "Course 2",
             city: "Test City 2",
             state: "Test State 2",
-            org_id: testOrganizationId,
+            org_id: golferUser.orgId,
           },
           {
             name: "Course 3",
             city: "Test City 3",
             state: "Test State 3",
-            org_id: testOrganizationId,
+            org_id: golferUser.orgId,
           },
         ];
-    
         createdCourses = await Course.bulkCreate(courses);
-        adminToken = await helper.get_token_for("admin");
-        customerToken = await helper.get_token_for("testCustomer");
-        testManagerToken = await helper.get_token_for("testManager");
-        golferToken = await helper.get_token_for("golfer");
-        console.log({golferToken});
-        differentOrganizationCustomerToken = await helper.get_token_for(
-          "zongCustomer",
-        );
-
-        // console.log(createdCourses);
-      });
-    
-
-    it("should create the game successfully", async () => {
-      const user = jwt.decode(golferToken);
-      const params = {
-        mcId: createdCourses[0].id,
-        totalIdealShots: 5,
-        teeColor: 'red',
-      };
-      const expectedResponse = {
-        mcId: createdCourses[0].id,
-        totalIdealShots: 5,
-        teeColor: "red",
-        ownerId: user.id,
-        participantId: user.id,
-        participantName: user.name,
-        orgId: user.orgId,
-      }
-// expect.any(Object)
-
-      const response = await helper.post_request_with_authorization({
-        endpoint: "game",
-        token: golferToken,
-        params: params,
       });
 
-      expect(response.body.data).toEqual(expect.objectContaining(expectedResponse));
-      expect(response.status).toBe(200);
+    describe("Success", () => {
+      it("should create the game successfully when parameters are valid", async () => {
+        const params = {
+          mcId: createdCourses[0].id,
+          totalIdealShots: 5,
+          teeColor: 'Red',
+        };
+        const expectedResponse = {
+          mcId: createdCourses[0].id,
+          totalIdealShots: 5,
+          teeColor: "Red",
+          ownerId: golferUser.id,
+          participantId: golferUser.id,
+          participantName: golferUser.name,
+          orgId: golferUser.orgId,
+        }
+  
+        const response = await helper.post_request_with_authorization({
+          endpoint: "game",
+          token: golferToken,
+          params: params,
+        });
+  
+        expect(response.body.data).toEqual(expect.objectContaining(expectedResponse));
+        expect(response.status).toBe(200);
+      })
+    })
+    describe("Fail", () => {
+      it("should fail if course does not exist", async () => {
+        const wrongCourseId = -1;
+        const params = {
+          mcId: wrongCourseId,
+          totalIdealShots: 5,
+          teeColor: 'Red',
+        };
+        const expectedResponse = {
+          mcId: createdCourses[0].id,
+          totalIdealShots: 5,
+          teeColor: "Red",
+          ownerId: golferUser.id,
+          participantId: golferUser.id,
+          participantName: golferUser.name,
+          orgId: golferUser.orgId,
+        }
+  
+        const response = await helper.post_request_with_authorization({
+          endpoint: "game",
+          token: golferToken,
+          params: params,
+        });
+  
+        expect(response.body.data).toEqual(`Course Not Found${config.error_message_separator}404`);
+      })
+      it("should fail if total ideal shots are invalid", async () => {
+        const params = {
+          mcId: createdCourses[0].id,
+          totalIdealShots: -5,
+          teeColor: 'Red',
+        };
+        const expectedResponse = {
+          success: false,
+          data: expect.any(Object) 
+        }
+
+  
+        const response = await helper.post_request_with_authorization({
+          endpoint: "game",
+          token: golferToken,
+          params: params,
+        });
+        
+        expect(response.body).toEqual(expect.objectContaining(expectedResponse));
+        expect(response.body.data.errors.totalIdealShots).toEqual(["The totalIdealShots must be at least 1."]);
+        expect(response.statusCode).toEqual(400);
+      })
+
+      it("should fail if tee color is not string", async () => {
+        const params = {
+          mcId: createdCourses[0].id,
+          totalIdealShots: 5,
+          teeColor: 321,
+        };
+        const expectedResponse = {
+          success: false,
+          data: expect.any(Object) 
+        }
+
+  
+        const response = await helper.post_request_with_authorization({
+          endpoint: "game",
+          token: golferToken,
+          params: params,
+        });
+        
+        expect(response.body).toEqual(expect.objectContaining(expectedResponse));
+        expect(response.body.data.errors.teeColor).toEqual(['The teeColor must be a string.']);
+        expect(response.statusCode).toEqual(400);
+      })
+
     })
 })
