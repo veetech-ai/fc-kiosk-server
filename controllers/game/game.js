943bf4fb0432c2e08234662bd30e2110e019e750
Maaz <maaz@veetech.ai>
2023-06-01 15:47:19 +0500

    feat: create holes

--- controllers/game/game.js
+++ controllers/game/game.js
@@ -3,6 +3,7 @@ const Validator = require("validatorjs");
 
 const apiResponse = require("../../common/api.response");
 const gameService = require("../../services/game/game");
+const holeService = require("../../services/game/hole");
 const courseServices = require("../../services/mobile/courses");
 const { v4: uuidv4 } = require("uuid");
 
@@ -16,7 +17,7 @@ exports.create_game = async (req, res) => {
   /**
    * @swagger
    *
-   * /game/:
+   * /game:
    *   post:
    *     security:
    *       - auth: []
@@ -31,15 +32,21 @@ exports.create_game = async (req, res) => {
    *         required: true
    *         type: string
    *       - name: totalIdealShots
-   *         description: Course ID
+   *         description: Total Ideal Shots
    *         in: body
    *         required: true
    *         type: string
    *       - name: teeColor
-   *         description: Course ID
+   *         description: Color of the tee
    *         in: body
    *         required: true
    *         type: string
+   *       - name: holes
+   *         example: [{ holeId: 1, holeNumber: 1, par: 5 }]
+   *         description: array of holes. The hole object properties are should be same as in the example
+   *         in: body
+   *         required: true
+   *         type: array
    *     produces:
    *       - application/json
    *     responses:
@@ -51,6 +58,7 @@ exports.create_game = async (req, res) => {
       mcId: "required|integer",
       totalIdealShots: "required|min:1|integer",
       teeColor: "required|string",
+      holes: "required|array",
     });
 
     if (validation.fails()) {
@@ -68,8 +76,11 @@ exports.create_game = async (req, res) => {
     req.body.startTime = new Date();
     req.body.gameId = uuidv4();
     req.body.orgId = req.user.orgId;
+    const holes = req.body.holes;
+    delete req.body.holes;
 
-    const createdGame = await gameService.createGame(req?.body)
+    const createdGame = await gameService.createGame(req?.body);
+    await holeService.createGameHoles(holes, req.user.id, createdGame.id, req.body.mcId);
     return apiResponse.success(res, req, createdGame);
   } catch (error) {
     return apiResponse.fail(res, error.message, error.statusCode || 500);
