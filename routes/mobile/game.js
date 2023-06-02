4d0ad7d10442fd2725726eba64a4b6caf2725bb0
Maaz <maaz@veetech.ai>
2023-06-02 14:44:16 +0500

    feat: repositioning

--- routes/mobile/game.js
+++ routes/mobile/game.js
@@ -1,6 +1,6 @@
-const config = require("../config/config");
-const GameController = require("../controllers/game/game");
-const validation_middleware = require("../middlewares/auth.validation");
+const config = require("../../config/config");
+const GameController = require("../../controllers/mobile/game");
+const validation_middleware = require("../../middlewares/auth.validation");
 
 exports.routesConfig = function (app, router) {
   const game = `${config.app.apiPath}game`;
