981c65b76aaa6e4c048fad8d7fe6db44ae9a81b2
Abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2023-08-02 15:18:13 +0500

    events table migration

---
+++ migrations/20230802080343-create-events-table.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Events", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      openingTime: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      closingTime: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        onUpdate: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("Events");
+  },
+};
