1c904a240ee227dc1068871226400412995c5b39
Abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2023-08-02 15:42:27 +0500

    added event modal

---
+++ models/events.js
@@ -0,0 +1,50 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("Event", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    openingTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    closingTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    corousal: {
+      type: DataTypes.JSON,
+      allowNull: true,
+      description: "Array of URLs of images",
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+    details: {
+      // There's open discussion here: whether to use VARCHAR(255), MEDIUMTEXT or LONGTEXT
+      type: DataTypes.LONGTEXT,
+      allowNull: true,
+      defaultValue: null,
+      description: "A rich text log description in HTML format",
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  });
+};
