cc65a19bafa1a5a4e21311d2261dfe68cff26d00
Abdullah-yasir-veetech <abdullah.yasir@veetech.ai>
2023-08-09 19:20:34 +0500

    added tiles migration

---
+++ migrations/20230809125221-create-tiles-table.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return Promise.all([
+      await queryInterface.createTable("Tiles", {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+          onUpdate: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+      }),
+    ]);
+  },
+
+  async down(queryInterface) {
+    return queryInterface.dropTable("Tiles");
+  },
+};
