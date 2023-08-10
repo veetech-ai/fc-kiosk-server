411b844a596531b57977b5700ddc45ef02d074ee
Abdul Munim <abdul.munim@veetech.ai>
2023-08-10 19:51:51 +0500

    chore: thumbnail check removed

--- controllers/kiosk/events.js
+++ controllers/kiosk/events.js
@@ -128,9 +128,9 @@ exports.createEvent = async (req, res) => {
       throw new ServiceError(validation.firstError(), 400);
     }
 
-    if (!files.thumbnail) {
-      throw new ServiceError("The thumbnail image is required.", 400);
-    }
+    // if (!files.thumbnail) {
+    //   throw new ServiceError("The thumbnail image is required.", 400);
+    // }
 
     // to throw error if course is not found
     await courseService.getCourseById(fields.gcId);
@@ -205,7 +205,6 @@ exports.updateEvent = async (req, res) => {
    *       - name: thumbnail
    *         description: thumbnail image for event
    *         in: formData
-   *         required: true
    *         type: file
    *
    *       - name: openingTime
