453e56854ae00c32fb695d524729674674989445
Abdullah-yasir-veetech <abdullah.yasir@veetech.ai>
2023-08-16 16:55:18 +0500

    chore: refactor

--- test/unit/controllers/kiosk/tiles/patch-tiles.test.js
+++ test/unit/controllers/kiosk/tiles/patch-tiles.test.js
@@ -1,14 +1,14 @@
-describe("PATCH /tiles", () => {
-  describe("/tiles/{id}/order", () => {
-    describe("success", () => {});
-    describe("failure", () => {});
-  });
-  describe("/tiles/{id}/super", () => {
-    describe("success", () => {});
-    describe("failure", () => {});
-  });
-  describe("/tiles/{id}/publish", () => {
-    describe("success", () => {});
-    describe("failure", () => {});
-  });
-});
+// describe("PATCH /tiles", () => {
+//   describe("/tiles/{id}/order", () => {
+//     describe("success", () => {});
+//     describe("failure", () => {});
+//   });
+//   describe("/tiles/{id}/super", () => {
+//     describe("success", () => {});
+//     describe("failure", () => {});
+//   });
+//   describe("/tiles/{id}/publish", () => {
+//     describe("success", () => {});
+//     describe("failure", () => {});
+//   });
+// });
