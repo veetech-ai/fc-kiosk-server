5cc22ec2390b4a93186e1ca0e005f90b3b947c1e
Abdullah Yasir <56272322+teckmk@users.noreply.github.com>
2024-09-24 00:04:56 +0500

    fix: remove references to iotcore-common

--- common/influx-init.js
+++ common/influx-init.js
@@ -1,18 +1,20 @@
 const { influx: influxConfig } = require("../config/config.js");
 const { logger } = require("../logger");
-const { Influx } = require("@cowlar/iotcore-common");
+// const { Influx } = require("@cowlar/iotcore-common");
 class InfluxGlobal {
   static #instance = null;
 
   constructor() {
     logger.info("*** ONBOARDING ***");
 
-    this.db = new Influx({
-      url: `http://${influxConfig.host}:${influxConfig.port}/`,
-      token: influxConfig.token,
-      organization: influxConfig.organization,
-      timeout: Number.parseInt(influxConfig.timeout),
-    });
+    // this.db = new Influx({
+    //   url: `http://${influxConfig.host}:${influxConfig.port}/`,
+    //   token: influxConfig.token,
+    //   organization: influxConfig.organization,
+    //   timeout: Number.parseInt(influxConfig.timeout),
+    // });
+
+    this.db = {};
   }
 
   static getInstance() {
